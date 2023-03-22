71aad6b334fe1a3500d45cb9841e247d9ac9ac9d
Borys <transkarpation@gmail.com>
2023-03-22 17:16:39 +0200

    listen to acc change

--- ethora-polygon-next/src/constants/config.ts
+++ ethora-polygon-next/src/constants/config.ts
@@ -1,4 +1,5 @@
 export const config = {
   'ethoraCoinAddress': process.env.ETHORA_COIN_ADDRESS || '0x1d0563fa48B6F83F290A9C7B79CdbFa8fD273d04',
-  'networkId': process.env.NETWORK_ID || '8001'
+  'networkId': process.env.NETWORK_ID || '8001',
+  'networkIdHex': process.env.NETWORK_ID_HEX || '0x13881'
 }
--- ethora-polygon-next/src/store/index.ts
+++ ethora-polygon-next/src/store/index.ts
@@ -2,11 +2,13 @@ import {create} from 'zustand'
 import { CartSlice, createCartSlice } from './slices/createCartSlice'
 import { createProductSlice, ProductSlice } from './slices/createProductSlice'
 import { createUserSlice, UserSlice } from './slices/userSlice'
+import { createAppSlice, AppSlice } from './slices/appSlice'
 
-type StoreState = ProductSlice & CartSlice & UserSlice
+type StoreState = ProductSlice & CartSlice & UserSlice & AppSlice
 
 export const useAppStore = create<StoreState>()((...a) => ({
     ...createProductSlice(...a),
     ...createCartSlice(...a),
-    ...createUserSlice(...a)
+    ...createUserSlice(...a),
+    ...createAppSlice(...a)
 }))
