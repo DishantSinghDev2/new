8eb477a65c57b7d5dafd032800cffcde330d27a7
Borys <transkarpation@gmail.com>
2023-03-28 18:57:26 +0300

    server auth mw

--- ethora-polygon-server/src/middleware/auth.ts
+++ ethora-polygon-server/src/middleware/auth.ts
@@ -1,9 +1,66 @@
-import {Request, Response, NextFunction} from 'express'
+import express, {Request, Response, NextFunction} from 'express'
+import * as sigUtil from '@metamask/eth-sig-util'
+import {SignTypedDataVersion} from '@metamask/eth-sig-util'
+import { format } from 'date-fns'
 
 function return401(res: Response) {
   return res.status(401).end()
 }
 
-export default function(req: Request, res: Response, next: NextFunction) {
-  const authHeader = req.header('authorization');
+function tokenParser(token: string) {
+  try {
+    return JSON.parse(Buffer.from(token, 'base64').toString())
+  } catch (e) {
+    return null
+  }
+}
+
+export type AuthRequest = express.Request & {
+  user: {
+    address: string
+  }
+}
+
+export function authMw(req: any, res: Response, next: NextFunction) {
+  const authHeader = req.header('authorization') as string
+
+  const parsed = tokenParser(authHeader)
+
+  if (parsed) {
+    const { ttl, sign, address } = parsed
+
+    if (!sign || ttl < Date.now()) {
+      return return401(res)
+    }
+
+    const msgParams = {
+      domain: {
+        name: "EthoraPolygonDev",
+        version: "1",
+      },
+      message: {
+        message: `Ethora Polygon Dev Sign In\n\nToken valid till: ${format(new Date(ttl), 'yyyy-MM-dd HH:mm aa')}`,
+      },
+      primaryType: "Login",
+      types: {
+        EIP712Domain: [
+          { name: "name", type: "string" },
+          { name: "version", type: "string" },
+        ],
+        Login: [{ name: "message", type: "string" }],
+      },
+    };
+    try {
+      const recoveredAddress = sigUtil.recoverTypedSignature({ data: msgParams as any, signature: '0x' + sign as string, version: SignTypedDataVersion.V4 } )
+
+      if (recoveredAddress.toLowerCase() === address.toLowerCase()) {
+        req.user = {
+          address
+        }
+        next()
+      }
+    } catch (error) {
+      return return401(res)
+    }
+  }
 }
--- ethora-polygon-server/src/router/index.ts
+++ ethora-polygon-server/src/router/index.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express'
 import Nfmt from '../db/models/nfmt'
+import {authMw, AuthRequest} from '../middleware/auth'
 
 const router = Router()
 
@@ -11,6 +12,12 @@ router.get('/nfmt', async (req, res) => {
   const nfmt = await Nfmt.find({})
 
   return res.send(nfmt)
+})
+
+router.get('/test-auth', authMw, (req: any, res) => {
+  console.log(req.user)
+
+  return res.send(req.user.address)
 })
 
 export default router
