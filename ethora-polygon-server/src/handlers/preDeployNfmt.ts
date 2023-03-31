c969e8a7ec10a9a46d0af8452e1a9d3e19bd6b19
Borys <transkarpation@gmail.com>
2023-03-31 11:52:46 +0300

    catch errors

--- ethora-polygon-server/src/handlers/preDeployNfmt.ts
+++ ethora-polygon-server/src/handlers/preDeployNfmt.ts
@@ -26,59 +26,69 @@ const bodySchema = Joi.object({
 }).required();
 
 export async function preDeployNfmtHandler(req: any, res: Response) {
-  // validations start
-  const error = bodySchema.validate(req.body).error;
+  try {
+    // validations start
+    const error = bodySchema.validate(req.body).error;
 
-  if (error) {
-    return res.status(422).send({ error });
-  }
-
-  if (!req.files || req.files.length === 0) {
-    return res.status(422).send({ error: 'should at least one file with image type' })
-  }
-
-  for (const file of req.files) {
-    if (file.mimetype.split("/")[0] !== "image") {
-      return res.status(422).send({ error: "only image files acceptable" });
+    if (error) {
+      return res.status(422).send({ error });
     }
-  }
-  // validations end
 
-  const { collectionName, collectionDescription } = req.body
+    if (!req.files || req.files.length === 0) {
+      return res
+        .status(422)
+        .send({ error: "should at least one file with image type" });
+    }
 
-  let imagesIpfsLinks: string[] = [];
+    for (const file of req.files) {
+      if (file.mimetype.split("/")[0] !== "image") {
+        return res.status(422).send({ error: "only image files acceptable" });
+      }
+    }
+    // validations end
 
-  for (const file of req.files) {
-    const rs = fs.createReadStream(path.resolve(file.path));
-    let ipfsUploadResult;
+    const { collectionName, collectionDescription } = req.body;
 
-    try {
-      ipfsUploadResult = await pinata.pinFileToIPFS(rs, {
-        pinataMetadata: { name: file.originalname },
+    let imagesIpfsLinks: string[] = [];
+
+    for (const file of req.files) {
+      const rs = fs.createReadStream(path.resolve(file.path));
+      let ipfsUploadResult;
+
+      try {
+        ipfsUploadResult = await pinata.pinFileToIPFS(rs, {
+          pinataMetadata: { name: file.originalname },
+        });
+      } catch (error) {
+        console.log(error);
+      }
+
+      const hash = ipfsUploadResult?.IpfsHash;
+      const ipfsUrl = config.pinataGateway + hash;
+      imagesIpfsLinks.push(ipfsUrl);
+    }
+
+    let metadataUrls: string[] = [];
+
+    for (const [index, imageLink] of imagesIpfsLinks.entries()) {
+      const metadata = {
+        name: `${collectionName} #${index + 1}`,
+        image: imageLink,
+        description: collectionDescription,
+      };
+
+      const res = await pinata.pinJSONToIPFS(metadata, {
+        pinataMetadata: {
+          name: `${collectionName} #${index + 1} ${Date.now()}`,
+        },
       });
-    } catch (error) {
-      console.log(error);
+      const hash = res?.IpfsHash;
+      const ipfsUrl = config.pinataGateway + hash;
+      metadataUrls.push(ipfsUrl);
     }
 
-    const hash = ipfsUploadResult?.IpfsHash;
-    const ipfsUrl = config.pinataGateway + hash;
-    imagesIpfsLinks.push(ipfsUrl);
+    return res.send({ ok: true, imagesIpfsLinks, metadataUrls });
+  } catch (error) {
+    return res.status(500).send(error)
   }
-
-  let metadataUrls: string[] = []
-
-  for (const [index, imageLink] of imagesIpfsLinks.entries()) {
-    const metadata = {
-      name: `${collectionName} #${index + 1}`,
-      image: imageLink,
-      description: collectionDescription
-    }
-
-    const res = await pinata.pinJSONToIPFS(metadata, {pinataMetadata: { name:  `${collectionName} #${index + 1} ${Date.now()}`},})
-    const hash = res?.IpfsHash;
-    const ipfsUrl = config.pinataGateway + hash;
-    metadataUrls.push(ipfsUrl);
-  }
-
-  return res.send({ ok: true, imagesIpfsLinks, metadataUrls });
 }
